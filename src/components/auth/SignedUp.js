3d672d61d9b52fd07a21eb21d19d60983beb12a2
seintun <seintun.edu@gmail.com>
2018-11-19 22:10:46 -0800

    added LogIn & Register component with form

--- src/components/auth/SignedUp.js
+++ src/components/auth/SignedUp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
-class SignedIn extends Component {
+class SignUp extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    firstName: "",
+    lastName: ""
   }
   handleChange = (e) => {
     this.setState({
@@ -18,7 +20,15 @@ class SignedIn extends Component {
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-1">Sign In</h5>
+          <h5 className="grey-text text-darken-1">Sign Up Form</h5>
+            <div className="input-field">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" id="firstName" onChange={this.handleChange}/>
+            </div>
+            <div className="input-field">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id="lastName" onChange={this.handleChange}/>
+            </div>
             <div className="input-field">
               <label htmlFor="email">Email</label>
               <input type="email" id="email" onChange={this.handleChange}/>
@@ -28,7 +38,7 @@ class SignedIn extends Component {
               <input type="password" id="password" onChange={this.handleChange}/>
             </div>
             <div className="input-field">
-              <button className="btn pink lighten-1 z-depth-0">Login</button>
+              <button className="btn pink lighten-1 z-depth-0">Register</button>
             </div>
         </form>
       </div>
@@ -36,4 +46,4 @@ class SignedIn extends Component {
   }
 }
 
-export default SignedIn
+export default SignUp
--- src/components/projects/ProjectSumamry.js
+++ src/components/projects/ProjectSumamry.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ProjectSumamry() {
   return (
-    <div className="card z-depth-0 project-summary">
+    <div className="card z-depth-1 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">Project Title</span>
         <p>Posted by the Net Ninja</p>
