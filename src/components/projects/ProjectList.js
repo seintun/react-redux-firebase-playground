2f63a95ec7800d2f217a8ab0de1af934f178e1f6
seintun <seintun.edu@gmail.com>
2018-11-19 19:51:23 -0800

    added projectList card components

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function ProjectList() {
+  return (
+    <div>
+      <div className="project-list section">
+
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by the Net Ninja</p>
+            <p className="grey-text">3rd September, 2 am</p>
+          </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by the Net Ninja</p>
+            <p className="grey-text">3rd September, 2 am</p>
+          </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by the Net Ninja</p>
+            <p className="grey-text">3rd September, 2 am</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
