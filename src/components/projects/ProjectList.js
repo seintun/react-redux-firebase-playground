6254d45e78121aaf7ac4de52d5e05471ceddb777
seintun <seintun.edu@gmail.com>
2018-11-19 19:54:53 -0800

    moved project template

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,34 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSumamry'
 
 export default function ProjectList() {
   return (
     <div>
       <div className="project-list section">
-
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by the Net Ninja</p>
-            <p className="grey-text">3rd September, 2 am</p>
-          </div>
-        </div>
-
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by the Net Ninja</p>
-            <p className="grey-text">3rd September, 2 am</p>
-          </div>
-        </div>
-
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by the Net Ninja</p>
-            <p className="grey-text">3rd September, 2 am</p>
-          </div>
-        </div>
-        
+        <ProjectSummary />
+        <ProjectSummary />
+        <ProjectSummary />
       </div>
     </div>
   )
