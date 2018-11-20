6d9efaaf9d0e5c7c6bae85c7f35a5ecfe0a7d205
seintun <seintun.edu@gmail.com>
2018-11-20 09:47:33 -0800

    Added new content project through mapDispatchToProps>dispatch>projectActions with async promise of adding new data into fireBase>projectReducer for switch case

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,9 +1,24 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to the database
-    dispatch({ 
-      type: 'CREATE_PROJECT', 
-      project
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Sein',
+      authorLastName: 'Tun',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ 
+        type: 'CREATE_PROJECT', 
+        project
+      })
+    }).catch((err) => {
+      dispatch({ 
+        type: 'CREATE_PROJECT_ERROR', 
+        err
+      })
     })
+
   }
 }
