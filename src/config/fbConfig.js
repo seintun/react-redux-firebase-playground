e24c337bad724527ca22a5a6e1f51859b2b36055
seintun <seintun.edu@gmail.com>
2018-11-20 01:11:15 -0800

    added firebase with config of auth & firestore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDgX9RH-V6JBxobar7_o0KX54wSHQKhLWw",
+  authDomain: "sein-ninja-marioplan.firebaseapp.com",
+  databaseURL: "https://sein-ninja-marioplan.firebaseio.com",
+  projectId: "sein-ninja-marioplan",
+  storageBucket: "sein-ninja-marioplan.appspot.com",
+  messagingSenderId: "624790006645"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().setting({ timestampInSnapshots: true })
+
+export default firebase;
