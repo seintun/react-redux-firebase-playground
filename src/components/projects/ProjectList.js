44c58a0425b4e6b90a85761d64764a891770c7df
seintun <seintun.edu@gmail.com>
2018-11-20 10:59:36 -0800

    added clickable for each project with Link for redirect

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 export default function ProjectList({projects}) {
   return (
     <div>
       <div className="project-list section">
-        { projects && projects.map(project =>
-            <ProjectSummary  key={project.id} project={project}/>
-        )}
+        { projects && projects.map(project => {
+          return (
+            <Link to={'/projects/' + project.id}>
+              <ProjectSummary  key={project.id} project={project}/>          
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
